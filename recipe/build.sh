9b8bac849e15b540a8231f53eb38360b9faba61e
Caleb Zulawski <caleb.zulawski@caci.com>
2023-03-16 15:03:17 -0400

    Account for cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-./configure --prefix=${PREFIX}
-make
-make check
+set -euxo pipefail
+
+./configure --prefix=${PREFIX}        \
+            --libdir=${PREFIX}/lib    \
+            --build=${BUILD}          \
+            --host=${HOST}
+
+make -j${CPU_COUNT}
+if [[ "$build_platform" == "$target_platform" ]]; then
+  make check
+fi
 make install
